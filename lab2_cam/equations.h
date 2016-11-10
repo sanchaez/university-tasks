53e9490eec93610868b251ba972b4ce0cb2a5e10
Oleksandr Shaposhnikov <o.shaposhnikov-2020@kpi.ua>
2016-11-10 19:00:57 +0200

    cam_lab2: Iterative calculus

--- lab2_cam/equations.h
+++ lab2_cam/equations.h
@@ -8,28 +8,61 @@ namespace eq {
 		Equation() = delete;
 		explicit Equation(FuncType func_default, 
 					      FuncType derivative_default, 
-						  double range_low, double range_high)
+						  const double& range_low, const double& range_high)
 		{
 			_func = func_default;
 			_derivative = derivative_default;
 			_range_a = range_low;
 			_range_b = range_high;
 		}
-		double operator()(double x) { return _func(x); }
-		double derivative(double x) { return _derivative(x); }
-		double iterative(double x, double accuracy) {
-			return 0.0;
-		}
-		double hordes(double x, double accuracy) {
-			return 0.0;
-		}
+		double operator()(const double& x) { return _func(x); }
+		double derivative(const double& x) { return _derivative(x); }
 		double get_iterations()		{ return _iterations_count;	}
+		//
+		double iterative(const double& precision) {
+			const double lambda_constant = lambda_factor();
+			const double q_constant = q_factor();
+
+			std::function<double(double)> phi_function;
+			if (lambda_constant < 0) {
+				phi_function = [&](double x)->double { return x - lambda_constant * _func(x); };
+			}
+			else {
+				phi_function = [&](double x)->double { return x + lambda_constant * _func(x); };
+			}
+			const double precision_constant = abs((1 - q_constant) / q_constant * precision);
+			double x0 = (_range_b - _range_a) / 1.7, xn = phi_function(x0);
+			double dbg = abs(xn - x0);
+			while (abs(xn - x0) > precision_constant) {
+				x0 = xn;
+				xn = phi_function(xn);
+			};
+			return xn;
+		}
+
+		double hordes(const double& accuracy) {
+			return 0.0;
+		}
 	private:
-		double lambda() {
-			double max_a = derivative(_range_a), 
-				max_b = derivative(_range_b);
-			return (max_a >= max_b) ? 1 / max_a : 1 / max_b;
+		double min_derivative_range() {
+			double min_a = _derivative(_range_a), 
+				min_b = _derivative(_range_b);
+			return (min_a <= min_b) ? min_a : min_b;
+		}
+
+		double max_derivative_range() {
+			double max_a = _derivative(_range_a), 
+				max_b = _derivative(_range_b);
+			return (max_a >= max_b) ? max_a :  max_b;
+		}
+
+		double lambda_factor() {
+			return 1 / max_derivative_range();
 		};
+
+		double q_factor() {
+			return 1 - min_derivative_range() / max_derivative_range();
+		}
 		FuncType *_func, *_derivative;
 		double _range_a;
 		double _range_b;
--- lab2_cam/main.cpp
+++ lab2_cam/main.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
 #include <iomanip>
+#include <initializer_list>
 #include "equations.h"
 //20: ln(x)- x + 5=0; I, H
 //[a, b] = [0, 1]
+
 double fn(double x) {
 	return log(x) - x + 5;
 }
@@ -11,9 +13,26 @@ double derivative(double x) {
 	return 1 / x - 1;
 }
 
+template <class T> 
+inline void fixed_width_print_obj(T obj, const int& width = 15, const int& precision = 15) {
+	std::cout << std::left << std::setw(width) << std::setprecision(precision) << obj;
+}
+
+template <class T> 
+void fixed_width_print_line(std::initializer_list<T> list, const int& width = 15) {
+	for (T obj : list) {
+		fixed_width_print_obj<T>(obj, width);
+	}
+	std::cout << std::endl;
+}
+
+
 void main(void) {
-	eq::Equation<double(double)> equations(&fn, &derivative, 0, 1);
-	std::cout << std::setw(10) << equations(5)
-		<<std::setw(10) << fn(5);
+	eq::Equation<double(double)> equations(&fn, &derivative, 5, 7);
+	//iterative test results
+	fixed_width_print_line({ ":precision", ":iterative" });
+	for (double precision = 1e-2; precision >= 1e-14; precision *= 1e-3) {
+		fixed_width_print_line({ precision, equations.iterative(precision) });
+	}
 	std::cin.get();
 }
