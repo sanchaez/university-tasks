23203ecb7e332d5e5ed99b73929f188791f811d9
Oleksandr Shaposhnikov <o.shaposhnikov-2020@kpi.ua>
2017-01-06 01:08:22 +0200

    cam_lab3: Redone iteration function to accept eps.

--- lab3_cam/main.cpp
+++ lab3_cam/main.cpp
@@ -1,7 +1,7 @@
-#include "print_helpers.h"
-#include "solver.h"
 #include <fstream>
 #include <iostream>
+#include "print_helpers.h"
+#include "solver.h"
 
 // TODO: experiment with iteration method described here (second method)
 //: http://orloff.am.tpu.ru/chisl_metod/Lab3/iter.htm
@@ -12,38 +12,33 @@ const solver::matrix_type<double> my_matrix = {{13, 14, 17, 14, 146},
                                                {8, 4, 24, 11, 119},
                                                {15, 11, 5, 16, 109}};
 
-const solver::matrix_type<double> iteration_matrix = {{13, 2, 3, 4, 146},
-                                                      {9, 26, 7, 9, 135},
-                                                      {8, 4, 24, 11, 119},
-                                                      {8, 11, 5, 36, 109}};
+//testing trivial matrix
+// {1,1,1,1}
+const solver::matrix_type<double> iteration_matrix = {
+    {100, 5, 30, 11, 146}, {5, 100, 20, 10, 135}, {5, 10, 100, 4, 119}, {5, 9, 5, 90, 109}};
 
 const unsigned int iterations_count_max = 100;
 
-solver::result_type<double>
-calculation_error(const solver::matrix_type<double>& matrix,
-                  const solver::result_type<double>& result)
-{
-  solver::index_type          matrix_size = matrix.size();
-  solver::index_type          result_size = result.size();
+solver::result_type<double> calculation_error(
+    const solver::matrix_type<double>& matrix,
+    const solver::result_type<double>& result) {
+  solver::index_type matrix_size = matrix.size();
+  solver::index_type result_size = result.size();
   solver::result_type<double> errors(result_size);
 
-  for (solver::index_type i = 0; i < matrix_size; ++i)
-  {
+  for (solver::index_type i = 0; i < matrix_size; ++i) {
     errors[i] = matrix[i][result_size];
-    for (solver::index_type j = 0; j < result_size; ++j)
-    {
+    for (solver::index_type j = 0; j < result_size; ++j) {
       errors[i] -= matrix[i][j] * result[j];
     }
   }
-  return errors.apply(fabs);
+  return abs(errors);
 }
 
-int main()
-{
+int main() {
   std::cout << "Input matrix" << std::endl;
   fixed_width_print_line({":ai1", ":ai2", ":ai3", ":ai4", ":bi"}, 10);
   fixed_width_print_matrix(my_matrix, 10);
-
 
   std::cout << std::endl << "Roots (selection method):" << std::endl;
   auto x = solver::selection(my_matrix);
@@ -61,22 +56,21 @@ int main()
   fixed_width_print_obj("x:", 5);
   fixed_width_print_line(y);
   fixed_width_print_obj("e:", 5);
-  fixed_width_print_line(calculation_error(iteration_matrix, y));
+  auto eps = calculation_error(iteration_matrix, y);
+  fixed_width_print_line(eps);
 
-  std::cout << std::endl << "Roots (iteration):" << std::endl;
-  fixed_width_print_obj("it:", 5);
-  fixed_width_print_line({":x1", ":x2", ":x3", ":x4"});
-  auto transformed_iteration_matrix = solver::prepare_iteration(iteration_matrix);
-  for (unsigned int iterations_count = 5;
-       iterations_count <= iterations_count_max; iterations_count += 5)
-  {
-    auto z = solver::iteration(transformed_iteration_matrix, iterations_count);
-    fixed_width_print_obj(iterations_count, 5);
-    fixed_width_print_line(z);
-    fixed_width_print_obj("err:", 5);
-    fixed_width_print_line(calculation_error(iteration_matrix, z));
+  auto eps_iter = eps.min();
+  //fallback
+  if (!eps_iter) {
+    eps_iter = 1e-15;
   }
-
+  std::cout << std::endl
+            << "Roots (iteration): epsilon = " << eps_iter << std::endl;
+  auto z = solver::iteration(iteration_matrix, eps_iter);
+  fixed_width_print_obj("x:", 5);
+  fixed_width_print_line(z);
+  fixed_width_print_obj("e:", 5);
+  fixed_width_print_line(calculation_error(iteration_matrix, z));
   std::cin.get();
   return 0;
 }
--- lab3_cam/solver.h
+++ lab3_cam/solver.h
@@ -1,9 +1,8 @@
 #pragma once
-#include "print_helpers.h"
 #include <valarray>
+#include "print_helpers.h"
 
-namespace solver
-{
+namespace solver {
 
 /*
   System of linear equations solver
@@ -17,52 +16,45 @@ using row_type = std::valarray<T>;
 template <typename T>
 using result_type = row_type<T>;
 template <typename T>
-using matrix_type      = std::valarray<row_type<T>>;
-using index_type       = std::size_t;
+using matrix_type = std::valarray<row_type<T>>;
+using index_type = std::size_t;
 using row_indexes_type = row_type<index_type>;
-using row_mask_type    = row_type<bool>;
+using row_mask_type = row_type<bool>;
 
 // main element selection method
 template <typename T = double>
-result_type<T> selection(const matrix_type<T>& coeff_matrix)
-{
+result_type<T> selection(const matrix_type<T>& coeff_matrix) {
   // copy input
-  matrix_type<T>   input_matrix = coeff_matrix;
+  matrix_type<T> input_matrix = coeff_matrix;
   const index_type roots_number = input_matrix.size();
-  row_mask_type    used_rows_mask(true, input_matrix.size()),
+  row_mask_type used_rows_mask(true, input_matrix.size()),
       used_columns_mask(true, input_matrix[0].size());
 
   // get triangle matrix using main element selection
-  matrix_type<T>   sorted_triangle_matrix(roots_number);
+  matrix_type<T> sorted_triangle_matrix(roots_number);
   row_indexes_type deleted_columns(roots_number);
 
   for (index_type triangle_matrix_index = roots_number - 1;
        triangle_matrix_index != static_cast<index_type>(-1);
-       --triangle_matrix_index)
-  {
+       --triangle_matrix_index) {
     // get max element pos (get main row)
     index_type first_index_i = 0, first_index_j = 0;
-    while (!used_rows_mask[first_index_i])
-    {
+    while (!used_rows_mask[first_index_i]) {
       ++first_index_i;
     }
-    while (!used_columns_mask[first_index_j])
-    {
+    while (!used_columns_mask[first_index_j]) {
       ++first_index_j;
     }
 
     index_type max_row_pos = first_index_i, max_column_pos = first_index_j;
-    T          max_value = input_matrix[first_index_i][first_index_j];
-    for (index_type i = first_index_i; i < input_matrix.size(); ++i)
-    {
-      if (used_rows_mask[i])
-      {
-        for (index_type j = first_index_j; j < input_matrix[i].size() - 1; ++j)
-        {
-          if (used_columns_mask[j] && input_matrix[i][j] > max_value)
-          {
-            max_value      = input_matrix[i][j];
-            max_row_pos    = i;
+    T max_value = input_matrix[first_index_i][first_index_j];
+    for (index_type i = first_index_i; i < input_matrix.size(); ++i) {
+      if (used_rows_mask[i]) {
+        for (index_type j = first_index_j; j < input_matrix[i].size() - 1;
+             ++j) {
+          if (used_columns_mask[j] && input_matrix[i][j] > max_value) {
+            max_value = input_matrix[i][j];
+            max_row_pos = i;
             max_column_pos = j;
           }
         }
@@ -71,15 +63,13 @@ result_type<T> selection(const matrix_type<T>& coeff_matrix)
 
     // save main row
     sorted_triangle_matrix[triangle_matrix_index] = input_matrix[max_row_pos];
-    deleted_columns[triangle_matrix_index]        = max_column_pos;
+    deleted_columns[triangle_matrix_index] = max_column_pos;
 
     // add main row to mask
     used_rows_mask[max_row_pos] = false;
     // substract calculated row from main matrix rows
-    for (index_type idx = 0; idx < input_matrix.size(); ++idx)
-    {
-      if (used_rows_mask[idx])
-      {
+    for (index_type idx = 0; idx < input_matrix.size(); ++idx) {
+      if (used_rows_mask[idx]) {
         auto buffer = static_cast<row_type<T>>(
                           input_matrix[max_row_pos][used_columns_mask]) *
                       (input_matrix[idx][max_column_pos] / (max_value));
@@ -93,16 +83,13 @@ result_type<T> selection(const matrix_type<T>& coeff_matrix)
 
   // calculate roots
   result_type<T> roots(T(0), roots_number);
-  for (index_type i = 0; i < roots_number; ++i)
-  {
+  for (index_type i = 0; i < roots_number; ++i) {
     auto column_size = sorted_triangle_matrix[i].size() - 1;
-    T    buffer      = sorted_triangle_matrix[i][column_size];
+    T buffer = sorted_triangle_matrix[i][column_size];
 
     for (index_type column_index = 0; column_index < column_size;
-         ++column_index)
-    {
-      if (column_index != deleted_columns[i])
-      {
+         ++column_index) {
+      if (column_index != deleted_columns[i]) {
         buffer -= roots[column_index] * sorted_triangle_matrix[i][column_index];
       }
     }
@@ -115,52 +102,68 @@ result_type<T> selection(const matrix_type<T>& coeff_matrix)
 // simple iterations method
 // diagonal elements assumed to be nonzero
 template <typename T = double>
-result_type<T> iteration(const matrix_type<T>& transformed_matrix,
-                         const unsigned int&   iterations_num)
-{
+result_type<T> iteration(const matrix_type<T>& coeff_matrix, const T& eps) {
+  matrix_type<T> transformed_matrix = prepare_iteration(coeff_matrix);
   const index_type roots_number = transformed_matrix.size();
-  result_type<T>   roots(T(0), roots_number);
-  // starting vector
-  // Roots for iterations
-  result_type<T> tmp_roots(T(0), roots_number);
 
-  for (index_type i = 0; i < static_cast<index_type>(iterations_num); ++i)
-  {
-    for (index_type j = 0; j < roots_number; ++j)
-    {
-      tmp_roots[j] = transformed_matrix[j][0];
-      for (index_type k = 0; k < roots_number; ++k)
-      {
-        tmp_roots[j] += roots[k] * transformed_matrix[j][k + 1];
-      }
-    }
-    roots = tmp_roots;
+
+  T q = m_norm_it(transformed_matrix);
+  if (q >= 1) throw std::exception("Matrix m norm more or equal than 1");
+
+  result_type<T> beta_vector(roots_number);
+  for (index_type i = 0; i < roots_number; ++i) {
+    beta_vector[i] = transformed_matrix[i][0];
   }
+  result_type<bool> alpha_values_mask(true, roots_number + 1);
+  alpha_values_mask[0] = false;
+  result_type<T> tmp_roots(T(0), roots_number);
+  result_type<T> roots(beta_vector);
+
+  const double precision_constant = ((1 - q) / q) * eps;
+  double norm_of_difference;
+  do {
+    tmp_roots = beta_vector;
+    for (index_type i = 0; i < roots_number; ++i) {
+      tmp_roots[i] += static_cast<result_type<T>>(roots *
+        static_cast<result_type<T>>(transformed_matrix[i][alpha_values_mask])).sum();
+    }
+    norm_of_difference = abs(static_cast<row_type<T>>(roots - tmp_roots)).sum();
+    roots = tmp_roots;
+  } while (norm_of_difference > precision_constant);
   return roots;
 }
 
+// private fn
+namespace {
 template <typename T = double>
-matrix_type<T> prepare_iteration(const matrix_type<T>& coeff_matrix)
-{
-  const index_type matrix_rows    = coeff_matrix.size();
+matrix_type<T> prepare_iteration(const matrix_type<T>& coeff_matrix) {
+  const index_type matrix_rows = coeff_matrix.size();
   const index_type matrix_columns = coeff_matrix[0].size();
-  matrix_type<T>   transformed_matrix(row_type<T>(matrix_columns), matrix_rows);
-  for (index_type i = 0; i < matrix_rows; ++i)
-  {
+  matrix_type<T> transformed_matrix(row_type<T>(matrix_columns), matrix_rows);
+  for (index_type i = 0; i < matrix_rows; ++i) {
     transformed_matrix[i][0] =
         coeff_matrix[i][matrix_columns - 1] / coeff_matrix[i][i];
-    for (index_type j = 0; j < matrix_columns - 1; ++j)
-    {
-      if (i != (j))
-      {
+    for (index_type j = 0; j < matrix_columns - 1; ++j) {
+      if (i != (j)) {
         transformed_matrix[i][j + 1] = -coeff_matrix[i][j] / coeff_matrix[i][i];
-      }
-      else
-      {
+      } else {
         transformed_matrix[i][j + 1] = 0;
       }
     }
   }
   return transformed_matrix;
+}
+
+template <typename T = double>
+T m_norm_it(matrix_type<T> matrix) {
+  const auto roots_num = matrix.size();
+  row_type<T> m_norm_sums(roots_num);
+  row_type<bool> mask(true, roots_num + 1);
+  mask[0] = false;
+  for (index_type i = 0; i < roots_num; ++i) {
+    m_norm_sums[i] = abs(static_cast<row_type<T>>(matrix[i][mask])).sum();
+  }
+  return m_norm_sums.max();
+}
 }
 }
